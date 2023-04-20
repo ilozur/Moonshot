e275bb324ce784428bc5a3555ce45321bdd82506
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-20 14:55:53 +0300

    Add Overview

---
+++ Moonshot/Overview.swift
@@ -0,0 +1,20 @@
+//
+//  Overview.swift
+//  Moonshot
+//
+//  Created by Edmond Podlegaev on 20.04.2023.
+//
+
+import SwiftUI
+
+struct Overview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Overview_Previews: PreviewProvider {
+    static var previews: some View {
+        Overview()
+    }
+}
