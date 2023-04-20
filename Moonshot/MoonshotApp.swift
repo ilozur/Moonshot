4b79495da92f6caa8bf45475a79a2129106d6437
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-20 14:53:53 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Edmond Podlegaev on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
